8ef4aa16794efb6381fd817790f9203fb83a03b2
Dasol Lim <dlim67@uwo.ca>
2023-09-21 00:25:16 -0400

    update search method and style

--- script.js
+++ script.js
@@ -1,86 +1,117 @@
-const countryData = [];
+// Store country names and currency codes
+var countryData = [];
+var currencyCodeData = [];
 
-// Get all elements with class name "country"
-const countryElements = document.querySelectorAll('.country');
+// DOMContentLoaded process web contents and prevent 'undefined' contents 
+document.addEventListener("DOMContentLoaded", function () {
 
-// Loop through the country elements and extract data
-countryElements.forEach(countryElement => {
-    const nameElement = countryElement.querySelector('.name');
-    const currencyElement = countryElement.querySelector('.currency');
+    // Select all class name "country"
+    var countryElements = document.querySelectorAll('.country');
 
-    if (nameElement && currencyElement) {
-        const name = nameElement.textContent.trim();
-        const currency = currencyElement.textContent.trim();
+    // Loop through the country elements
+    countryElements.forEach(countryElement => {
+        var nameElement = countryElement.querySelector('.name');
+        var currencyElement = countryElement.querySelector('.currency');
 
-        // Push the extracted data into the countryData array
-        countryData.push({ name, currency });
-    }
+        // Store found country name or currency code
+        if (nameElement) {
+            var name = nameElement.textContent.trim();
+
+            // Push the extracted data into the countryData array
+            countryData.push(name);
+        }
+
+        if (currencyElement) {
+            var currency = currencyElement.textContent.trim();
+
+            // Push the extracted data into the currencyCodeData array
+            currencyCodeData.push(currency);
+        }
+    });
+
 });
 
-alert("Name: " + countryData[0].name + "\nCurrency: " + countryData[0].currency);
-
-
-function validateCountryName() {
+function searchCountryName() {
     var searchInput = document.getElementById("countryName");
     var countryName = searchInput.value.trim();
 
-    // Check if the input is empty
-    if (countryName === '') {
-        alert('Please enter a country name.');
-        return false;
+    // Create an array to store matching countries
+    var matchingCountries = [];
+
+    // Check for matches and add
+    if (!countryName == "") {
+        countryData.forEach(function (country) {
+            if (country.toLowerCase().includes(countryName.toLowerCase())) {
+                matchingCountries.push(country);
+            }
+        });
     }
 
-    // Check if the input contains any numbers
-    if (/\d/.test(countryName)) {
-        alert('Country name cannot contain numbers.');
-        return false;
+    // Display alert
+    if (matchingCountries.length > 0) {
+        alert("Countries Found:\n" + matchingCountries.join("\n"));
+    } else {
+        alert("No matching countries found.");
     }
 
     /*
     // Check if the input is longer than 20 characters
     if (countryName.length > 20) {
-        alert('Country name should be no more than 20 characters.');
-        return false;
+        alert('Error: Country name should be no more than 20 characters');
     }
     */
-
-    // If all validations pass, you can perform your search or other actions here.
-    return true;
 }
 
-// Function to validate the currency code input
-function validateCurrencyCode() {
-    const currencyCodeInput = document.getElementById('currencyCode');
-    const currencyCode = currencyCodeInput.value.trim();
+// Function to search the currency code input
+function searchCurrencyCode() {
+    var currencyCodeInput = document.getElementById('currencyCode');
+    var currencyCode = currencyCodeInput.value.trim();
 
-    // Check if the input is empty
-    if (currencyCode === '') {
-        alert('Please enter a currency code.');
-        return false;
+    // Create an array to store matching country codes
+    var matchingCurrencyCodes = [];
+
+    // Check for matches and add
+    if (!currencyCode == "") {
+
+        currencyCodeData.forEach(function (code) {
+
+            if (code.toLowerCase().includes(currencyCode.toLowerCase())) {
+                matchingCurrencyCodes.push(code);
+            }
+        });
     }
 
-    // Check if the input consists of three upper case letters A-Z
-    if (!/^[A-Z]{3}$/.test(currencyCode)) {
-        alert('Currency code should be three upper case letters (A-Z).');
-        return false;
+    // Display alert
+    if (matchingCurrencyCodes.length > 0) {
+        alert("Countries Found:\n" + matchingCurrencyCodes.join("\n"));
+    } else {
+        alert("No matching countries found.");
     }
 
-    // If all validations pass, you can perform your search or other actions here.
-    return true;
+    /*
+    // Check if the input is longer than 3 characters
+    if (currencyCode.length > 3) {
+        alert('Error: Currency code should be no more than 3 characters');
+    }
+    */
 }
 
-// Handle Enter key press for country name input
-document.getElementById('countryName').addEventListener('keydown', function (event) {
-    if (event.key === 'Enter') {
-        event.preventDefault();
-        validateCountryName();
-    }
+// Allow Enter key press to search country name
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('countryName').addEventListener('keydown', function (event) {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            searchCountryName();
+        }
+    });
 });
 
-// Handle Enter key press for currency code input
-document.getElementById('currencyCode').addEventListener('keydown', function (event) {
-    if (event.key === 'Enter') {
-        event.preventDefault();
-        validateCurrencyCode();
-    }
+// Allow Enter key press to search currency code
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('currencyCode').addEventListener('keydown', function (event) {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            searchCurrencyCode();
+        }
+    });
 });
